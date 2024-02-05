d546430aad9a8fb7d770ac1aa322b40611e73443
uelionobre <uelionobre@hotmail.com>
2024-02-05 11:57:43 -0300

    Adiciona viewsets de Music, Playlist e Singer a aplicacao core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Music, Playlist, Singer
+from .serializers import MusicSerializer, PlaylistSerializer, SingerSerializer
 
-# Create your views here.
+
+class MusicViewSet(viewsets.ModelViewSet):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+
+
+class PlaylistViewSet(viewsets.ModelViewSet):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+
+
+class SingerViewSet(viewsets.ModelViewSet):
+    queryset = Singer.objects.all()
+    serializer_class = SingerSerializer
