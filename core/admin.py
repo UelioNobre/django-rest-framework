e16ca74e3a6cf79ebf6d14d4193248f7d6c76aad
uelionobre <uelionobre@hotmail.com>
2024-02-05 11:37:21 -0300

    Registra os models de Singer, Music e Playlists em Core/admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Singer, Music, Playlist
+
+admin.site.register(Singer)
+admin.site.register(Music)
+admin.site.register(Playlist)
